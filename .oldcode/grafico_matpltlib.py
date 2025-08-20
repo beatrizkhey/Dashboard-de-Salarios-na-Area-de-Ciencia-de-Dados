64b6a2175fc5b1e64832f11a93963b6425779bcb
beatrizkhey <beatrizkhey@gmail.com>
2025-08-20 20:34:52 -0300

    Página final
    Signed-off-by: beatrizkhey <beatrizkhey@gmail.com>

--- .oldcode/grafico_matpltlib.py
+++
@@ -1,8 +0,0 @@
-import matplotlib.pyplot as plt
-
-plt.figure(figsize=(8, 5))
-sns.barplot(data=df_limpo, x='senioridade', y='usd', estimator='mean')
-plt.title('Salário médio anual por nível de senioridade')
-plt.ylabel('Salário mádio (USD)')
-plt.xlabel('Senioridade')
-plt.show()
